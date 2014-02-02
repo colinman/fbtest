b401bb36b1911c923471442f826abb911dc30d1f
Ubuntu <ubuntu@ip-172-31-36-49.us-west-2.compute.internal>
2014-02-02 12:25:43 +0000

    jk last submit had a bug that i just fixed

--- fbtest.js
+++ fbtest.js
@@ -5,8 +5,29 @@ var requestsent = function(req, res) {
     var collection = db.get('user_images');
 
     if(req.method == 'POST') {
-	collection.update({user_id:req.query["user_id"]}, {$set:{img_urls:req.query["img_urls"]}}, {upsert:true}, function() {console.log("Finished adding images for " + req.query["user_id"]);});
+	var images = JSON.parse(req.query["img_urls"]);
+	collection.update({user_id:parseInt(req.query["user_id"])}, {$set:{img_urls:images}}, {upsert:true}, function() {console.log("Finished adding images for " + req.query["user_id"]);});
     }
 }
 
+var getrequest = function(req, res) {
+    var mongo = require('mongodb');
+    var monk = require('monk');
+    var db = monk('localhost:27017/fbtest');
+    var collection = db.get('user_images');
+
+    collection.find({user_id:parseInt(req.query["user_id"])}, {}, function(e, docs) {
+	if(docs[0] != undefined) {
+	    res.write(JSON.stringify(docs[0]["img_urls"]));
+	    console.log("data sent for " + req.query["user_id"]);
+	}
+	else {
+	    res.write("userdne");
+	    console.log("user does not exist: " + req.query["user_id"]);
+	}
+	res.end();
+    });
+}
+
 module.exports.requestsent = requestsent;
+module.exports.getrequest = getrequest;
