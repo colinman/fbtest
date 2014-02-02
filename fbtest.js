0cdc0eb2275f932ecc0a7b10fdd49169da9ac436
Ubuntu <ubuntu@ip-172-31-36-49.us-west-2.compute.internal>
2014-02-02 10:32:11 +0000

    showing images in lightbox working; about to add webservice to save user images

--- fbtest.js
+++ fbtest.js
@@ -1,10 +1,3 @@
-var express = require('express');
-var routes = require('./routes');
-var http = require('http');
-var mongo = require('mongodb');
-var monk = require('monk');
-var db = monk('localhost:27017/nodetest1');
-
 var requestsent = function(req, res) {
     res.sendfile('index.html');
 }
