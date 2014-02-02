3d65b3175e29a334c56ff9b70932ca4bc04fd4e0
Ubuntu <ubuntu@ip-172-31-36-49.us-west-2.compute.internal>
2014-02-02 11:22:35 +0000

    finished web service to process post requests with images for each user and store data in database

--- fbtest.js
+++ fbtest.js
@@ -1,5 +1,12 @@
 var requestsent = function(req, res) {
-    res.sendfile('index.html');
+    var mongo = require('mongodb');
+    var monk = require('monk');
+    var db = monk('localhost:27017/fbtest');
+    var collection = db.get('user_images');
+
+    if(req.method == 'POST') {
+	collection.update({user_id:req.query["user_id"]}, {$set:{img_urls:req.query["img_urls"]}}, {upsert:true}, function() {console.log("Finished adding images for " + req.query["user_id"]);});
+    }
 }
 
 module.exports.requestsent = requestsent;
